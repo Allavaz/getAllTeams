a02e032a247e0361e6e3eceeb75f8d14275532b2
David Zavalla <davidzavalla@gmail.com>
2019-11-11 19:07:10 -0300

    Add file open dialog

--- index.js
+++ index.js
@@ -4,41 +4,69 @@ const encuser = encodeURIComponent(user);
 const encpw = encodeURIComponent(pw);
 const authMechanism = 'DEFAULT';
 const url = `mongodb://${encuser}:${encpw}@${host}:27017/?authMechanism=${authMechanism}`;
-const Teams = require('./Teams');
+const {app, dialog} = require('electron');
+const fs = require('fs');
+
 let missingTeams = [];
 
-console.log('Connecting to database...');
-
-MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
-	const db = client.db(dbname);
-	db.collection('matchesaux').aggregate(
-		[{$project: {
-			'teams.teamname': 1
-		}}, {$unwind: {
-			path: '$teams',
-			includeArrayIndex: 'i',
-			preserveNullAndEmptyArrays: true
-		}}, {$group: {
-			_id: '$teams.teamname',
-		}}]
-	)
-		.sort({_id: 1})
-		.toArray((err, docs) => {
-			docs.forEach((item) => {
-				try {
-					Teams[item._id].toLowerCase();
-				} catch(e) {
-					missingTeams.push(item._id);
-				} 
-			});
-			client.close();
-			if (missingTeams.length === 0) {
-				console.log('No missing teams for now.');
-			} else {
-				console.log('Missing teams:');
-				missingTeams.forEach((item) => {
-					console.log(item);
+app.on('ready', () => {
+	dialog.showOpenDialog(
+		{filters: [
+			{name:'JSON', extensions: ['json']}
+		]},
+		{properties: ['openFile']}
+	).then((result) => {
+		if (result.canceled) {
+			app.exit(0);
+		} else {
+			console.log('Connecting to database...\n');
+			let path = result.filePaths[0];
+			try {
+				let data = fs.readFileSync(path);
+				let json = JSON.parse(data);
+				MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
+					const db = client.db(dbname);
+					db.collection('matchesaux').aggregate(
+						[{$project: {
+							'teams.teamname': 1
+						}}, {$unwind: {
+							path: '$teams',
+							includeArrayIndex: 'i',
+							preserveNullAndEmptyArrays: true
+						}}, {$group: {
+							_id: '$teams.teamname',
+						}}]
+					)
+						.sort({_id: 1})
+						.toArray((err, docs) => {
+							docs.forEach((item) => {
+								try {
+									json[item._id].toLowerCase();
+								} catch(e) {
+									try {
+										missingTeams.push(item._id);
+									} catch(e) {
+										console.error(e);
+										app.exit(0);
+									}
+								} 
+							});
+							client.close();
+							if (missingTeams.length === 0) {
+								console.log('No missing teams for now.');
+							} else {
+								console.log('Missing teams:');
+								missingTeams.forEach((item) => {
+									console.log(item);
+								});
+							}
+							app.exit(0);
+						});
 				});
+			} catch(e) {
+				console.error(e);
+				app.exit(0);
 			}
-		});
+		}
+	});
 });
